339a388c5e8798681ecb9d92bb96f5049e516a0f
eulegang <eulegang@eulegang.dev>
2024-11-08 15:57:28 -0600

    Adding writer/reader methods to File type

--- src/arg_types/file.zig
+++ src/arg_types/file.zig
@@ -11,4 +11,33 @@ pub const File = union(enum) {
             return File{ .file = arg };
         }
     }
+
+    pub fn reader(self: @This()) !std.io.Reader {
+        switch (self) {
+            .std => {
+                return std.io.getStdIn().reader();
+            },
+
+            .file => |f| {
+                const file = try std.fs.cwd().openFile(f);
+                return file.reader();
+            },
+        }
+    }
+
+    pub fn writer(self: @This()) !std.io.Writer {
+        switch (self) {
+            .std => {
+                return std.io.getStdOut().writer();
+            },
+
+            .file => |f| {
+                const file = try std.fs.cwd().openFile(f, .{
+                    .mode = .write_only,
+                });
+
+                return file.writer();
+            },
+        }
+    }
 };
